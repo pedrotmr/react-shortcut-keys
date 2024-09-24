e45f1a84b873a671e72b1f4ad5e5c009c443c15a
Pedro Ramos <pedrotr@hotmail.com>
2024-09-24 14:54:40 -0300

    update gtiignore file

--- .changeset/silly-shrimps-juggle.md
+++
@@ -1,5 +0,0 @@
----
-"react-keyboard-shortcuts": patch
----
-
-small change
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as Shortcut } from "./Shortcut";
 export { default as useShortcut } from "./use-shortcut";
-const a = 1;
